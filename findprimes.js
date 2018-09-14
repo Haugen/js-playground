d690f25346b09c93ba64b57b0953136250f1213c
Tobias Haugen <hej@tobiashaugen.se>
2018-09-14 12:32:28 +0200

    Another variant to find primes.

--- findprimes.js
+++ findprimes.js
@@ -1,10 +1,28 @@
 "use strict";
 
-next: for (let i = 2; i < 100; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) {
-      continue next;
-    }
+// next: for (let i = 2; i < 100; i++) {
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) {
+//       continue next;
+//     }
+//   }
+//   console.log(i);
+// }
+
+function findPrimes(number = 100) {
+  let primes = "";
+
+  for (let i = 1; i <= number; i++) {
+    primes += isPrime(i) || "";
   }
-  console.log(i);
+
+  return primes;
 }
+
+function isPrime(number) {
+  for (let i = 2; i < number; i++) {
+    return (number % i == 0) ? false : number + " ";
+  }
+}
+
+console.log(findPrimes());
