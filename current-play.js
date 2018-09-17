6867c1ef6c90422a0524ea08151bd2e1700ac761
Tobias Haugen <hej@tobiashaugen.se>
2018-09-17 10:46:37 +0200

    Committing old play

--- current-play.js
+++ current-play.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log(10);
+function findPrimes(x = 100) {
+  for (let i = 1; i <= x; i++) {
+    if (!isPrime(i)) continue;
+    console.log(i);
+  }
+}
+
+function isPrime(x) {
+  for (let i = 2; i < x; i++) {
+    if (x % i == 0) return false;
+  }
+  return true;
+}
+
+findPrimes();
