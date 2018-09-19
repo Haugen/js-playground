6e4dd7c0f85a83d3567711ee9f98285727bd5fcc
Tobias Haugen <hej@tobiashaugen.se>
2018-09-19 22:15:14 +0200

    Some more array practice.

--- datatypes/array-tasks.js
+++ datatypes/array-tasks.js
@@ -30,3 +30,32 @@ function camelize2(str) {
     .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
     .join("");
 }
+
+//
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 6);
+l(filtered);
+
+function filterRange(arr, a, b) {
+  let newArr = arr.filter((item) => (item >= a && item <= b));
+  return newArr;
+}
+
+//
+
+arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 4, 10); // removed the numbers except from 4 to 10
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+l(arr);
