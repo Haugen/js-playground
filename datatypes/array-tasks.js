dc3ac9894ac0686566dbd657628392c1df1a12b8
Tobias Haugen <hej@tobiashaugen.se>
2018-09-20 10:21:51 +0200

    More array methods practice.

--- datatypes/array-tasks.js
+++ datatypes/array-tasks.js
@@ -59,3 +59,87 @@ function filterRangeInPlace(arr, a, b) {
 }
 
 l(arr);
+
+//
+
+arr = [5, 2, 1, -10, 8];
+arr.sort((a, b) => a - b).reverse();
+l(arr); // 8, 5, 2, 1, -10
+
+//
+
+arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted2(arr);
+
+// What I first came up with.
+function copySorted(arr) {
+  let newArr = [];
+  for (let key in arr) {
+    newArr[key] = arr[key];
+  }
+  return newArr.sort();
+}
+
+// Shorter solution.
+function copySorted2(arr) {
+  // Can ommit arguments for slice when copying whole array, but keeping it here
+  // for clarity.
+  return arr.slice(0, arr.length).sort();
+}
+
+l(sorted); // CSS, HTML, JavaScript
+l(arr); // HTML, JavaScript, CSS (no changes)
+
+//
+
+let users = [
+  { firstName: "Tobias", lastName: "Haugen", id: 1, age: 32 },
+  { firstName: "Anders", lastName: "Svensson", id: 2, age: 40 },
+  { firstName: "Fredrik", lastName: "Haugen", id: 3, age: 30 },
+  { firstName: "Adam", lastName: "Baga", id: 4, age: 25 }
+];
+
+let names = users.map(function(item) {
+  return item.firstName;
+});
+
+// Or shorter map for this case.
+names = users.map(item => item.firstName);
+
+l(names);
+
+//
+
+// More mapping practice.
+let usersMapped = users.map(item => ({
+  fullName: `${item.firstName} ${item.lastName}`,
+  id: item.id
+}));
+
+l(usersMapped);
+
+//
+
+l(sortByName(users));
+
+// Sort the user objects in "users" array by user age.
+function sortByName(arr) {
+  return arr.sort((a, b) => a.age > b.age);
+}
+
+//
+
+l(getAverageAge2(users));
+
+// What I first came up with.
+function getAverageAge(arr) {
+  let totalAge = 0;
+  arr.forEach(item => totalAge += item.age);
+  return totalAge / arr.length;
+}
+
+// The fancy pantsy one line solution.
+function getAverageAge2(users) {
+  return users.reduce((sum, current) => sum + current.age, 0) / users.length;
+}
