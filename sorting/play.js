ed964ee0fd542f22f9bbb48322d934dd824049b5
Tobias Haugen <hej@tobiashaugen.se>
2018-10-27 20:22:32 -0300

    First try at insertion sort.

--- sorting/play.js
+++ sorting/play.js
@@ -4,7 +4,34 @@ const l = console.log;
 
 // prettier-ignore
 const numbers = [1, 5, 6, 34, 6, 34, 7, 9, 10, 2, 6, 8, 30, 23, 76, 333, 234, 1, 3, 5];
-const numbersShort = [2, 4, 1];
+const numbersShort = [1, 2, 4, 1, 7];
+
+// My first try at insertion sort. A bit more challenging than bubble sort, and I'm
+// not sure if it's a
+function insertionSort(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (newArr.length === 0) {
+      newArr.push(arr[i]);
+      continue;
+    }
+    for (let j = 0; j < newArr.length; j++) {
+      if (arr[i] < newArr[j]) {
+        newArr.splice(j, 0, arr[i]);
+        break;
+      }
+      if (newArr.length === j + 1) {
+        newArr.push(arr[i]);
+        break;
+      }
+    }
+  }
+
+  return newArr;
+}
+
+console.log(insertionSort(numbers));
 
 // Trying out my own custom bubble sort.
 function bubbleSort(arr, swappedLast = true) {
@@ -21,8 +48,7 @@ function bubbleSort(arr, swappedLast = true) {
     }
   }
 
-  console.log(newArr);
   return bubbleSort(newArr, swapped);
 }
 
-console.log(bubbleSort(numbers));
+// console.log(bubbleSort(numbers));
