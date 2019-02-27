a91f58ed5697f5e9c4b7229843f9a9c3fe90c757
Tobias Haugen <hej@tobiashaugen.se>
2019-02-27 13:12:06 -0500

    More notes on prototypical inheritance.

--- objects/prototype-v2.js
+++ objects/prototype-v2.js
@@ -10,22 +10,29 @@ func.prototype.score = 0;
 
 let obj = new func('Tobias', 32);
 
-func.prototype.test = 'now it shows up?';
+// When anObject is created, its __proto__ is set to Object.prototype
+let anObject = {};
+anObject.__proto__; // Object prototype.
+anObject.__proto__ === Object.prototype; // true
+anObject instanceof Object; // true
+Object.prototype.isPrototypeOf(anObject); // true
 
-// When test is created, its __proto__ is set to Object.prototype
-let test = {};
-console.log(test.__proto__);
-console.log(Object.prototype);
-console.log(test.__proto__ === Object.prototype); // true
-console.log(test instanceof Object);
-console.log(Object.prototype.isPrototypeOf(test));
+// When anArray is created, its __proto__ is set to Array.prototype
+let anArray = [];
+anArray.__proto__; // Array prototype.
+anArray.__proto__.__proto__; // Object prototype.
+anArray.__proto__ === Array.prototype; // true
+anArray.__proto__.__proto__ === Object.prototype; // true
+Array.prototype.isPrototypeOf(anArray); // true
 
-let arr = [];
-console.log(arr);
-console.log(arr.__proto__); // Array
-console.log(arr.__proto__.__proto__); // Object
+// When aFunction is created, its __proto__ is set to Function.prototype.
+// It also gets a reference to an empty object, its prototype.
+let aFunction = function() {};
+aFunction.prototype; // The constructor function prototype Object.
+aFunction.prototype.prop = 'This is a property.'; // Add properties to the prototype.
+aFunction.prototype.__proto__; // Object prototype.
 
-let fu = () => {};
-console.log(fu);
-console.log(fu.__proto__); // Function
-console.log(fu.__proto__.__proto__); // Object
+aFunction.__proto__; // Function prototype, where bind, apply, etc lives.
+aFunction.__proto__ === Function.prototype; // true
+aFunction.__proto__.__proto__ === Object.prototype; // true
+Function.prototype.isPrototypeOf(aFunction); // true
