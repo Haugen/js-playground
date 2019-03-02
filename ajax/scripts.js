d2307e1207ad31af19ecf2c67ec60b861dbba51d
Tobias Haugen <hej@tobiashaugen.se>
2019-03-02 12:48:53 -0500

    Adding breed select to dog viewer.

--- ajax/scripts.js
+++ ajax/scripts.js
@@ -1,16 +1,22 @@
 'use strict';
 
-const DOG_URL = 'https://dog.ceo/api/breeds/image/random';
+const ORIGINAL_URL = 'https://dog.ceo/api/breeds/image/random';
+const BREEDS = 'https://dog.ceo/api/breeds/list/all';
 
-const l = console.log;
+let dogUrl = ORIGINAL_URL;
+
 const imgContainer = document.querySelector('.dog-container');
 const button = document.querySelector('.get-dog');
 const loader = document.querySelector('.loader');
-const images = [];
-const maxHeight = window.innerHeight - button.clientHeight - 20;
+const breedSelect = document.querySelector('#breeds');
+const breedContainer = document.querySelector('.breed-select');
+let images = [];
+const maxHeight =
+  window.innerHeight - button.clientHeight - breedContainer.clientHeight - 60;
 const maxWidth = window.innerWidth - 20;
 imgContainer.style.height = maxHeight + 'px';
 
+loadBreeds();
 pushDogs();
 
 // Push a number of dog URLs into dogs array.
@@ -20,9 +26,43 @@ function pushDogs() {
   }
 }
 
+async function loadBreeds() {
+  const response = await fetch(BREEDS);
+  if (breeds) {
+    const breeds = await response.json();
+    for (let [key, value] of Object.entries(breeds.message)) {
+      let option = document.createElement('option');
+      option.text = key.charAt(0).toUpperCase() + key.slice(1);
+      option.value = key;
+      breedSelect.append(option);
+      if (value.length > 0) {
+        value.forEach(subBreed => {
+          let subOption = document.createElement('option');
+          subOption.text =
+            '- ' + subBreed.charAt(0).toUpperCase() + subBreed.slice(1);
+          subOption.value = key + '/' + subBreed;
+          breedSelect.append(subOption);
+        });
+      }
+    }
+  }
+}
+
 // Print a new dog on every button click.
 button.addEventListener('click', function(event) {
   printDog();
+});
+
+// Listen on change of breed.
+breedSelect.addEventListener('change', function(event) {
+  const breed = event.target.value;
+  if (breed === 'all') {
+    dogUrl = 'https://dog.ceo/api/breeds/image/random';
+  } else {
+    dogUrl = `https://dog.ceo/api/breed/${breed}/images/random`;
+  }
+  images = [];
+  pushDogs();
 });
 
 // Now printDog removes an element, add a new one, removes an image from the cache,
@@ -37,7 +77,7 @@ function printDog() {
 
 // Get a new dog URL and create an image.
 function fetchDog() {
-  const dog = fetch(DOG_URL)
+  const dog = fetch(dogUrl)
     .then(response => response.json())
     .then(json => json.message)
     .then(url => preloadImage(url));
